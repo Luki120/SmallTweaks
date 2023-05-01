652cc022c0d53c53e31bdb99680427f8116053cb
luki120 <luki120@exerra.xyz>
2023-05-01 20:55:07 -0300

    [Elixir] added a new method

--- Apps/Elixir/Factory/ElixirLabelFactory.h
+++ Apps/Elixir/Factory/ElixirLabelFactory.h
@@ -4,5 +4,6 @@
 @interface ElixirLabelFactory : NSObject
 + (ElixirLabel *)makeElixirLabel;
 + (NSString *)tweakCountString;
++ (void)centerElixirLabelOnBothAxesOfView:(UIView *)view;
 + (void)pinElixirLabelToTheTopCenteredOnTheXAxisOfView:(UIView *)view;
 @end
--- Apps/Elixir/Factory/ElixirLabelFactory.m
+++ Apps/Elixir/Factory/ElixirLabelFactory.m
@@ -7,19 +7,27 @@ static ElixirLabel *elixirLabel;
 
 + (ElixirLabel *)makeElixirLabel {
 
-	elixirLabel = [ElixirLabel new];
+	if(!elixirLabel) elixirLabel = [ElixirLabel new];
 	return elixirLabel;
 
 }
 
 
 + (NSString *)tweakCountString { return elixirLabel.tweakCountString; }
-+ (void)pinElixirLabelToTheTopCenteredOnTheXAxisOfView:(UIView *)view {
++ (void)centerElixirLabelOnBothAxesOfView:(UIView *)view {
 
 	// Hi, if you've reached here, please do yourself a favor if this is your case.
 	// Stop doing cursed, weird af UIScreen calculations with frames for UI layout stuff, 
 	// learn and embrace constraints instead, they are natural and beautiful,
 	// also known as AutoLayout, AUTO (it does the thing for you!!!)
+	[elixirLabel.centerXAnchor constraintEqualToAnchor: view.centerXAnchor].active = YES;
+	[elixirLabel.centerYAnchor constraintEqualToAnchor: view.centerYAnchor].active = YES;
+
+}
+
+
++ (void)pinElixirLabelToTheTopCenteredOnTheXAxisOfView:(UIView *)view {
+
 	[elixirLabel.topAnchor constraintEqualToAnchor: view.topAnchor constant: 8].active = YES;
 	[elixirLabel.centerXAnchor constraintEqualToAnchor: view.centerXAnchor].active = YES;
 
