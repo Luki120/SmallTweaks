bff90e8e051249a4e5faab2c870e20711f2861cd
luki120 <luki120@exerra.xyz>
2024-04-06 20:40:28 -0300

    added Canna [no ci]

---
+++ SpringBoard/Canna/Tweak.m
@@ -0,0 +1,43 @@
+@import AudioToolbox;
+@import CydiaSubstrate;
+#import <rootless.h>
+
+
+@interface SBUIController: NSObject
+- (BOOL)_powerSourceWantsToPlayChime;
+@end
+
+
+@class SBSleepWakeHardwareButtonInteraction;
+
+#define jbRootPath(path) ROOT_PATH_NS(path)
+
+static void createSystemSoundFromPath(NSString *path) {
+
+	SystemSoundID sound = 0;
+	AudioServicesDisposeSystemSoundID(sound);
+	AudioServicesCreateSystemSoundID((__bridge CFURLRef) [NSURL fileURLWithPath: jbRootPath(path)], &sound);
+	AudioServicesPlaySystemSound((SystemSoundID) sound);
+
+}
+
+static void (*origPlayChargingChimeIfAppropriate)(SBUIController *, SEL);
+static void overridePlayChargingChimeIfAppropriate(SBUIController *self, SEL _cmd) {
+
+	if(![self _powerSourceWantsToPlayChime]) return origPlayChargingChimeIfAppropriate(self, _cmd);
+	createSystemSoundFromPath(@"/Library/Tweak Support/Canna/connect_power.caf");
+
+}
+
+static void overridePlayLockSound(SBSleepWakeHardwareButtonInteraction *self, SEL _cmd) {
+
+	createSystemSoundFromPath(@"/Library/Tweak Support/Canna/lock.caf");
+
+}
+
+__attribute__((constructor)) static void init(void) {
+
+	MSHookMessageEx(NSClassFromString(@"SBUIController"), @selector(playChargingChimeIfAppropriate), (IMP) &overridePlayChargingChimeIfAppropriate, (IMP *) &origPlayChargingChimeIfAppropriate);
+	MSHookMessageEx(NSClassFromString(@"SBSleepWakeHardwareButtonInteraction"), @selector(_playLockSound), (IMP) &overridePlayLockSound, (IMP *) NULL);
+
+}
