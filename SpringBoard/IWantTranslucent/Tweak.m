0f04fdfbed83fb74726e17eed3c50f8f8bb93315
luki120 <luki120@exerra.xyz>
2024-04-13 08:14:49 -0300

    [IWT] iOS 16+ support for context menu titles • Apple stop fucking making out with the context menu classes and renaming them for no fucking reason thank you • cleanup & misc improvements

--- SpringBoard/IWantTranslucent/Tweak.m
+++ SpringBoard/IWantTranslucent/Tweak.m
@@ -6,17 +6,7 @@ translucent look + hides separators system wide ---*/
 @import CydiaSubstrate;
 
 
-@interface UIView ()
-@property (nonatomic, strong) UIVisualEffectView *backgroundView;
-@property (nonatomic, strong) UIVisualEffectView *visualEffectView;
-@end
-
-
 @interface _UIContextMenuActionsListTitleView: UIView
-@end
-
-
-@interface CCUIMenuModuleItemView : UIView
 @end
 
 
@@ -25,26 +15,63 @@ translucent look + hides separators system wide ---*/
 @end
 
 
+@class CCUIMenuModuleItemView;
 @class _UIContextMenuTitleView;
 
 #define kClass(string) NSClassFromString(string)
 #define kOSVersion [[UIDevice currentDevice].systemVersion floatValue]
 
-// Translucency
-static void (*origContextMenuDMTW)(UIView *, SEL);
-static void overrideContextMenuDMTW(UIView *self, SEL _cmd) {
+// ! iOS 14+
 
-	origContextMenuDMTW(self, _cmd);
-	if(kOSVersion >= 15.0) self.backgroundView.alpha = 0.5;
-	else self.visualEffectView.alpha = 0.5;
+// Translucency
+static void (*origSetBackgroundView)(UIView *, SEL, UIVisualEffectView *);
+static void overrideSetBackgroundView(UIView *self, SEL _cmd, UIVisualEffectView *visualEffectView) {
+
+	origSetBackgroundView(self, _cmd, visualEffectView);
+	visualEffectView.alpha = 0.5;
 
 }
 
-static UIVisualEffectView *overrideContextMenuTitleBV(_UIContextMenuTitleView *self, SEL _cmd) {
+// Separators
+static void (*origContextMenuSeparatorDMTW)(UIView *, SEL);
+static void overrideContextMenuSeparatorDMTW(UIView *self, SEL _cmd) {
+
+	origContextMenuSeparatorDMTW(self, _cmd);
+	[self removeFromSuperview];
+
+}
+
+// Thick context menu separator
+static void (*origUICollectionReusableViewDMTW)(UICollectionReusableView *, SEL);
+static void overrideUICollectionReusableViewDMTW(UICollectionReusableView *self, SEL _cmd) {
+
+	origUICollectionReusableViewDMTW(self, _cmd);
+
+	NSString *reuseIdentifier;
+
+	if(kOSVersion >= 15.0) reuseIdentifier = @"kContextMenuSectionSeparator";
+	else reuseIdentifier = @"kContextMenuItemSeparator";
+
+	if(![self.reuseIdentifier isEqualToString: reuseIdentifier]) return;
+	self.hidden = YES;
+
+}
+
+// ! iOS 15, 16+
+
+static UIVisualEffectView *overrideTitleBackgroundView(UIView *self, SEL _cmd) {
 
 	return [UIVisualEffectView new];
 
 }
+
+static UIView *overrideTitleSeparator(UIView *self, SEL _cmd) {
+
+	return [UIView new];
+
+}
+
+// ! iOS 14
 
 static void (*origContextMenuTitleDMTW)(_UIContextMenuActionsListTitleView *, SEL);
 static void overrideContextMenuTitleDMTW(_UIContextMenuActionsListTitleView *self, SEL _cmd) {
@@ -58,37 +85,7 @@ static void overrideContextMenuTitleDMTW(_UIContextMenuActionsListTitleView *sel
 
 }
 
-// Hide context menu separators system wide
-static void (*origContextMenuSeparatorDMTW)(UIView *, SEL);
-static void overrideContextMenuSeparatorDMTW(UIView *self, SEL _cmd) {
-
-	origContextMenuSeparatorDMTW(self, _cmd);
-	[self removeFromSuperview];
-
-}
-
-// Context menu title separator
-static UIView *overrideContextMenuTitleSeparator(_UIContextMenuTitleView *self, SEL _cmd) {
-
-	return [UIView new];
-
-}
-
-// Thick context menu separator
-static void (*origUICollectionReusableViewDMTW)(UICollectionReusableView *, SEL);
-static void overrideUICollectionReusableViewDMTW(UICollectionReusableView *self, SEL _cmd) {
-
-	NSString *reuseIdentifier;
-
-	if(kOSVersion >= 15.0) reuseIdentifier = @"kContextMenuSectionSeparator";
-	else reuseIdentifier = @"kContextMenuItemSeparator";
-
-	if(![self.reuseIdentifier isEqualToString: reuseIdentifier]) return origUICollectionReusableViewDMTW(self, _cmd);
-
-	origUICollectionReusableViewDMTW(self, _cmd);
-	self.hidden = YES;
-
-}
+// ! Control Center
 
 // CC Bluetooth & WiFi expanded modules
 static CGFloat overrideSeparatorHeight(CCUIMenuModuleItemView *self, SEL _cmd) { return 0; }
@@ -110,15 +107,20 @@ static UIView *overrideSeparatorView(UIView *self, SEL _cmd) {
 
 __attribute__((constructor)) static void init(void) {
 
-	if(kOSVersion >= 15.0) {
-		MSHookMessageEx(kClass(@"_UIContextMenuListView"), @selector(didMoveToWindow), (IMP) &overrideContextMenuDMTW, (IMP *) &origContextMenuDMTW);
-		MSHookMessageEx(kClass(@"_UIContextMenuTitleView"), @selector(bgView), (IMP) &overrideContextMenuTitleBV, (IMP *) NULL);
-		MSHookMessageEx(kClass(@"_UIContextMenuTitleView"), @selector(separator), (IMP) &overrideContextMenuTitleSeparator, (IMP *) NULL);
+	if(kOSVersion >= 16.0) {
+		MSHookMessageEx(kClass(@"_UIContextMenuHeaderView"), @selector(bgView), (IMP) &overrideTitleBackgroundView, (IMP *) NULL);
+		MSHookMessageEx(kClass(@"_UIContextMenuHeaderView"), @selector(separator), (IMP) &overrideTitleSeparator, (IMP *) NULL);
+	}
+
+	else if(kOSVersion >= 15.0 && kOSVersion < 16.0) {
+		MSHookMessageEx(kClass(@"_UIContextMenuListView"), @selector(setBackgroundView:), (IMP) &overrideSetBackgroundView, (IMP *) &origSetBackgroundView);
+		MSHookMessageEx(kClass(@"_UIContextMenuTitleView"), @selector(bgView), (IMP) &overrideTitleBackgroundView, (IMP *) NULL);
+		MSHookMessageEx(kClass(@"_UIContextMenuTitleView"), @selector(separator), (IMP) &overrideTitleSeparator, (IMP *) NULL);
 		MSHookMessageEx(kClass(@"_UIContextMenuReusableSeparatorView"), @selector(didMoveToWindow), (IMP) &overrideContextMenuSeparatorDMTW, (IMP *) &origContextMenuSeparatorDMTW);
 	}
 
 	else {
-		MSHookMessageEx(kClass(@"_UIElasticContextMenuBackgroundView"), @selector(didMoveToWindow), (IMP) &overrideContextMenuDMTW, (IMP *) &origContextMenuDMTW);
+		MSHookMessageEx(kClass(@"_UIElasticContextMenuBackgroundView"), @selector(setVisualEffectView:), (IMP) &overrideSetBackgroundView, (IMP *) &origSetBackgroundView);
 		MSHookMessageEx(kClass(@"_UIContextMenuActionsListTitleView"), @selector(didMoveToWindow), (IMP) &overrideContextMenuTitleDMTW, (IMP *) &origContextMenuTitleDMTW);
 		MSHookMessageEx(kClass(@"_UIContextMenuActionsListSeparatorView"), @selector(didMoveToWindow), (IMP) &overrideContextMenuSeparatorDMTW, (IMP *) &origContextMenuSeparatorDMTW);
 	}
