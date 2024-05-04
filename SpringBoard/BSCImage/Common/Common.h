947570e77016e91628c4115a106b5d4884c14b32
luki120 <luki120@exerra.xyz>
2024-05-04 06:38:07 -0300

    gone

--- SpringBoard/BSCImage/Common/Common.h
+++
@@ -1,9 +0,0 @@
-#import <rootless.h>
-
-#define jbRootPath(path) ROOT_PATH_NS(path)
-#define kPath jbRootPath(@"/var/mobile/Library/Preferences/me.luki.bscimageprefs.plist")
-
-static NSNotificationName const BSCImageApplyImageNotification = @"BSCImageApplyImageNotification";
-
-@interface NSDistributedNotificationCenter : NSNotificationCenter
-@end
