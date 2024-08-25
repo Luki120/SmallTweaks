8efe84d565d96c8f355c0a9599198a6fc33f8047
luki120 <luki120@exerra.xyz>
2024-08-25 04:13:49 -0300

    [Apps] [Elixir] Windi malded

--- Apps/Elixir/Elixir.m
+++ Apps/Elixir/Elixir.m
@@ -9,7 +9,7 @@
 
 
 #define kClass(class) NSClassFromString(class)
-#define kIsCurrentApp(string) [[[NSBundle mainBundle] bundleIdentifier] isEqual: string]
+#define kIsCurrentApp(string) [[[NSBundle mainBundle] bundleIdentifier] isEqualToString: string]
 #define kOneSettings dlopen(jbRootPathC("/Library/MobileSubstrate/DynamicLibraries/OneSettings.dylib"), RTLD_LAZY)
 #define kOrion dlopen(jbRootPathC("/Library/MobileSubstrate/DynamicLibraries/OrionSettings.dylib"), RTLD_LAZY)
 #define kShuffle dlopen(jbRootPathC("/Library/MobileSubstrate/DynamicLibraries/shuffle.dylib"), RTLD_LAZY)
@@ -19,21 +19,34 @@
 @end
 
 
+@interface OrionTweakSpecifiersController : PSListController
+@end
+
+
 @interface TSRootListController : UIViewController
 @property (copy, nonatomic) NSString *title;
 @end
 
+// Orion
 
-@interface TweakSpecifiersController : PSListController
-@end
+static void (*origOrionVDL)(OrionTweakSpecifiersController *, SEL);
+static void overrideOrionVDL(OrionTweakSpecifiersController *self, SEL _cmd) {
 
+	origOrionVDL(self, _cmd);
 
-static void (*origShuffleVDL)(TweakSpecifiersController *, SEL);
-static void overrideShuffleVDL(TweakSpecifiersController *self, SEL _cmd) {
+	[self.table addSubview: [ElixirLabelFactory makeElixirLabel]];
+	[ElixirLabelFactory pinElixirLabelToTheTopCenteredOnTheXAxisOfView: self.table];
 
-	origShuffleVDL(self, _cmd);
+}
 
-	// Shuffle has a search bar so no space at the top :woeIsFade:
+// OneSettings & Shuffle
+
+static void (*origVDL)(PSListController *, SEL);
+static void overrideVDL(PSListController *self, SEL _cmd) {
+
+	origVDL(self, _cmd);
+
+	// OneSettings & Shuffle have a search bar so no space at the top :smh:
 	UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 40)];
 	[footerView addSubview: [ElixirLabelFactory makeElixirLabel]];
 	[ElixirLabelFactory centerElixirLabelOnBothAxesOfView: footerView];
@@ -42,15 +55,7 @@ static void overrideShuffleVDL(TweakSpecifiersController *self, SEL _cmd) {
 
 }
 
-static void (*origTweaksVDL)(PSListController *, SEL);
-static void overrideTweaksVDL(PSListController *self, SEL _cmd) {
-
-	origTweaksVDL(self, _cmd);
-
-	[self.table addSubview: [ElixirLabelFactory makeElixirLabel]];
-	[ElixirLabelFactory pinElixirLabelToTheTopCenteredOnTheXAxisOfView: self.table];
-
-}
+// TweakSettings
 
 static void (*origTSVDL)(TSRootListController *, SEL);
 static void overrideTSVDL(TSRootListController *self, SEL _cmd) {
@@ -62,10 +67,12 @@ static void overrideTSVDL(TSRootListController *self, SEL _cmd) {
 
 }
 
-static void (*origVDL)(PSUIPrefsListController *, SEL);
-static void overrideVDL(PSUIPrefsListController *self, SEL _cmd) {
+// Stock
 
-	origVDL(self, _cmd);
+static void (*origPSUIPrefsListControllerVDL)(PSUIPrefsListController *, SEL);
+static void overridePSUIPrefsListControllerVDL(PSUIPrefsListController *self, SEL _cmd) {
+
+	origPSUIPrefsListControllerVDL(self, _cmd);
 
 	PSSpecifier *emptySpecifier = [PSSpecifier emptyGroupSpecifier];
 
@@ -81,15 +88,15 @@ static void overrideVDL(PSUIPrefsListController *self, SEL _cmd) {
 __attribute__((constructor)) static void init(void) {
 
 	if(kOneSettings != nil)
-		MSHookMessageEx(kClass(@"OSTweaksListController"), @selector(viewDidLoad), (IMP) &overrideTweaksVDL, (IMP *) &origTweaksVDL);
+		MSHookMessageEx(kClass(@"OSTweaksListController"), @selector(viewDidLoad), (IMP) &overrideVDL, (IMP *) &origVDL);
 
 	else if(kOrion != nil)
-		MSHookMessageEx(kClass(@"OrionTweakSpecifiersController"), @selector(viewDidLoad), (IMP) &overrideTweaksVDL, (IMP *) &origTweaksVDL);
+		MSHookMessageEx(kClass(@"OrionTweakSpecifiersController"), @selector(viewDidLoad), (IMP) &overrideOrionVDL, (IMP *) &origOrionVDL);
 
 	else if(kShuffle != nil)
-		MSHookMessageEx(kClass(@"TweakPreferencesListController"), @selector(viewDidLoad), (IMP) &overrideShuffleVDL, (IMP *) &origShuffleVDL);
+		MSHookMessageEx(kClass(@"TweakPreferencesListController"), @selector(viewDidLoad), (IMP) &overrideVDL, (IMP *) &origVDL);
 
-	else MSHookMessageEx(kClass(@"PSUIPrefsListController"), @selector(viewDidLoad), (IMP) &overrideVDL, (IMP *) &origVDL);
+	else MSHookMessageEx(kClass(@"PSUIPrefsListController"), @selector(viewDidLoad), (IMP) &overridePSUIPrefsListControllerVDL, (IMP *) &origPSUIPrefsListControllerVDL);
 
 	if(!kIsCurrentApp(@"com.creaturecoding.tweaksettings")) return;
 	MSHookMessageEx(kClass(@"TSRootListController"), @selector(viewDidLoad), (IMP) &overrideTSVDL, (IMP *) &origTSVDL);
